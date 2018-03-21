7fce79534f524893aa6d6214797174dc5394dc04
Michael Bridgen <mikeb@squaremobius.net>
2018-03-21 14:03:36 +0000

    Database scaffolding

--- src/jepsen/etcd_demo.clj
+++ src/jepsen/etcd_demo.clj
@@ -1,12 +1,32 @@
 (ns jepsen.etcd-demo
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require
+   [clojure.tools.logging :refer :all]
+   [clojure.string :as str]
+   [jepsen
+    [control :as c]
+    [cli :as cli]
+    [db :as db]
+    [tests :as tests]]
+   [jepsen.control.util :as cu]
+   [jepsen.os.debian :as debian]))
+
+(defn db
+  "Etcd at given version"
+  [vsn]
+  (reify db/DB
+    (setup! [db test node]
+      (info :setting-up node))
+
+    (teardown! [db test node]
+      (info :tearing-down node))))
 
 (defn etcd-test
   "Parses command-line options and returns a test value"
   [opts]
-  (merge tests/noop-test ;; map merge
-         opts))
+  (merge tests/noop-test ;; map merge 
+         opts
+         {:os debian/os
+          :db (db "v3.1.5")}))
 
 (defn -main
   "Run jepsen test with command-line args"
