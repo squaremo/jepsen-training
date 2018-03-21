4f529b09414969ce1ec7c7849fc77f74a8eb679f
Michael Bridgen <mikeb@squaremobius.net>
2018-03-21 14:24:30 +0000

    Actually get an etcd cluster running

--- src/jepsen/etcd_demo.clj
+++ src/jepsen/etcd_demo.clj
@@ -10,20 +10,69 @@
    [jepsen.control.util :as cu]
    [jepsen.os.debian :as debian]))
 
+(def dir "/opt/etcd")
+(def binary "etcd")
+(def logfile (str dir "/etcd.log"))
+(def pidfile (str dir "/etcd.pid"))
+
+(defn node-url
+  [node port]
+  (str "http://" node ":" port))
+
+(defn peer-url
+  [node]
+  (node-url node 2380))
+
+(defn client-url
+  [node]
+  (node-url node 2379))
+
+(defn initial-cluster
+  "Construct an etcd argument giving the cluster nodes"
+  [test]
+  (->> (:nodes test)
+       (map (fn [node]
+              (str node "=" (peer-url node))))
+       (str/join ",")))
+
 (defn db
   "Etcd at given version"
   [vsn]
   (reify db/DB
     (setup! [db test node]
-      (info :setting-up node))
+      (info :setting-up node)
+      (c/su
+       (let [url (str "https://storage.googleapis.com/etcd/" vsn
+                      "/etcd-" vsn "-linux-amd64.tar.gz")]
+         (cu/install-archive! url dir))  ;; takes care of caching downloads, unpacking etc.
+       (cu/start-daemon! ;; takes care of daemonising etc.
+        {:logfile logfile
+         :pidfile pidfile
+         :chdir   dir}
+        binary
+        :--log-output                   :stderr
+        :--name                         (name node)
+        :--listen-peer-urls             (peer-url   node)
+        :--listen-client-urls           (client-url node)
+        :--advertise-client-urls        (client-url node)
+        :--initial-cluster-state        :new
+        :--initial-advertise-peer-urls  (peer-url node)
+        :--initial-cluster              (initial-cluster test)))
+      (Thread/sleep 10000))
 
     (teardown! [db test node]
-      (info :tearing-down node))))
+      (info :tearing-down node)
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir))) ;; it's the only way to be sure.
+
+    db/LogFiles
+    (log-files [db test node]
+      [logfile])))
 
 (defn etcd-test
   "Parses command-line options and returns a test value"
   [opts]
-  (merge tests/noop-test ;; map merge 
+  (merge tests/noop-test ;; map merge
          opts
          {:os debian/os
           :db (db "v3.1.5")}))
